d797e082966f011ccd73863e9aca1165c462ad60
kimyz <yeaji105@naver.com>
2024-01-30 17:20:51 +0900

    feat: redux-persist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,12 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from '@/redux/provider'
 import Header from '@/layouts/header/Header'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+import store from '@/redux/store'
 
 const inter = Inter({ subsets: ['latin'] })
+export let persistor = persistStore(store)
 
 export default function RootLayout({
   children,
@@ -15,9 +19,11 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Providers>
-          {' '}
-          <Header />
-          {children}
+          <PersistGate loading={null} persistor={persistor}>
+            {' '}
+            <Header />
+            {children}
+          </PersistGate>
         </Providers>
       </body>
     </html>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,23 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import postReducer from './slice/postSlice'
 import authReducer from './slice/authSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
 
 const rootReducer = combineReducers({
   posts: postReducer,
   auth: authReducer,
 })
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
